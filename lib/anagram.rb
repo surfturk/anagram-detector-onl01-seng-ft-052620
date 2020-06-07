1975ea210afb5580d1d0e2fa93e31130ea121c96
Berkley Brown <berkleydeane@gmail.com>
2020-06-07 23:02:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require "pry"
+
+ class Anagram
+  attr_accessor :word
+  
+  
+  def initialize(word)
+    @word = word
+    
+  end  
+  
+  def match(array)
+    #binding.pry
+      array.select do |array_word|
+      (@word.split("").sort) == (array_word.split("").sort)
+    end
+  end  
+  
+  
+  
+end  
