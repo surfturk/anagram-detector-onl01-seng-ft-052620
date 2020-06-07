35a01c47cc55ef2e2b28e41d08b5bfcda36a7c3e
Berkley Brown <berkleydeane@gmail.com>
2020-06-07 22:47:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   
   def match(array)
     array.select do |array_word|
-      (@word.split.("").sort) == 
+      (@word.split.("").sort) == (array_word.split)
     end  
   end  
   
